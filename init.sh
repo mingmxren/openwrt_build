fc9d602786b6df27552a520756162001211c2f45
mingmxren <renmingxv@gmail.com>
2022-02-11 14:58:24 +0800

    save

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -ex
 
-echo "deb http://mirrors4.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
-deb http://mirrors4.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
-deb http://mirrors4.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
-deb http://mirrors4.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse" > /etc/apt/sources.list
+echo "deb http://mirrors.ustc.edu.cn/ubuntu/ focal main restricted universe multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ focal-security main restricted universe multiverse" > /etc/apt/sources.list
 
 apt-get update
-apt-get install -y vim htop build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -yq vim htop build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
 apt-get clean
